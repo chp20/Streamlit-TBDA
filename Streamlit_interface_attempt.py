c9c7e211c85007204ea4fa64551320f96cb27da3
chp20 <96595434+chp20@users.noreply.github.com>
2023-11-30 18:15:26 +0100

    Update Streamlit_interface_attempt.py

--- Streamlit_interface_attempt.py
+++ Streamlit_interface_attempt.py
@@ -111,7 +111,7 @@ qry = "select * FROM `actividad-G02`"
 mycursor.execute(qry)
 rows = mycursor.fetchall()
 for x in rows:
-    if x[4] > date_begin and x[5] < date_end:
+    if x[4] > date_begin:
         st.write(x[4])
     else:
         print('no')
