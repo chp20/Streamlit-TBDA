f68bb5aff581f0461e3544d93b02b9bbc6d82662
chp20 <96595434+chp20@users.noreply.github.com>
2023-12-06 17:35:04 +0100

    Update Streamlit_interface_attempt.py

--- Streamlit_interface_attempt.py
+++ Streamlit_interface_attempt.py
@@ -122,8 +122,8 @@ for x in rows:
      #   st.write('no')
         
 st.write(df)
-check = [dt.datetime(2023, 1, 23, 11, 12, 54),dt.datetime(2023, 1, 23, 11, 13, 14)]
-plot = plt.plot_date(check,[1,1])
+check = [dt.datetime(2023, 1, 23, 11, 12, 54)]
+plot = plt.plot_date(check,1)
 st.pyplot(plot)
 
 
