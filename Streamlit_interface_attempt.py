d857885cd47a402925983c0d78ae4068e50ef71b
chp20 <96595434+chp20@users.noreply.github.com>
2023-12-11 15:48:09 +0100

    Update Streamlit_interface_attempt.py

--- Streamlit_interface_attempt.py
+++ Streamlit_interface_attempt.py
@@ -10,34 +10,34 @@ import matplotlib.pyplot as plt
 col1, col2, col3 = st.columns(3)
 
 #create time inputs
-with col1:
-    with st.expander('Begin date'):
-        begin_date = st.date_input('Give your begin date:')
-        begin_time = st.time_input('Begin time:')
+# with col1:
+#     with st.expander('Begin date'):
+#         begin_date = st.date_input('Give your begin date:')
+#         begin_time = st.time_input('Begin time:')
 
-with col2:
-    with st.expander('Final date'):
-        Final_date = st.date_input('Give your final date:')
-        Final_time = st.time_input('Final time:')
+# with col2:
+#     with st.expander('Final date'):
+#         Final_date = st.date_input('Give your final date:')
+#         Final_time = st.time_input('Final time:')
 
-#create boxes to let viewer choose what values he wants to see
-with col3:
-    with st.expander('Values to be plotted'):
-        S_bool = st.checkbox('S_Values')
-        a_bool = st.checkbox('a_values')
-        g_bool = st.checkbox('g_values')
-        m_bool = st.checkbox('m_values')
+# #create boxes to let viewer choose what values he wants to see
+# with col3:
+#     with st.expander('Values to be plotted'):
+#         S_bool = st.checkbox('S_Values')
+#         a_bool = st.checkbox('a_values')
+#         g_bool = st.checkbox('g_values')
+#         m_bool = st.checkbox('m_values')
 
         
-#create time intervals
-date_begin = dt.datetime.combine(begin_date, begin_time)
-date_end = dt.datetime.combine(Final_date, Final_time)
-interval_max = dt.timedelta(days=10)
-time_difference = date_end - date_begin
+# #create time intervals
+# date_begin = dt.datetime.combine(begin_date, begin_time)
+# date_end = dt.datetime.combine(Final_date, Final_time)
+# interval_max = dt.timedelta(days=10)
+# time_difference = date_end - date_begin
 
-#create boolean check for time interval chosen
-if time_difference > interval_max:
-    st.write('Its not possible to select more than 11 days.')
+# #create boolean check for time interval chosen
+# if time_difference > interval_max:
+#     st.write('Its not possible to select more than 11 days.')
 
 # def get_influx(mac, auth, dat, time_window):
 #     dat['From'] = dat.desde.dt.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
@@ -171,6 +171,38 @@ if boolean_decision:
 else:
     # If checkbox is not checked, create an empty placeholder
     placeholder = st.empty()
+
+    with col1:
+        with st.expander('Begin date'):
+            begin_date = st.date_input('Give your begin date:')
+            begin_time = st.time_input('Begin time:')
+
+    with col2:
+        with st.expander('Final date'):
+            Final_date = st.date_input('Give your final date:')
+            Final_time = st.time_input('Final time:')
+
+    #create boxes to let viewer choose what values he wants to see
+    with col3:
+        with st.expander('Values to be plotted'):
+            S_bool = st.checkbox('S_Values')
+            a_bool = st.checkbox('a_values')
+            g_bool = st.checkbox('g_values')
+            m_bool = st.checkbox('m_values')
+
+        
+    #create time intervals
+    date_begin = dt.datetime.combine(begin_date, begin_time)
+    date_end = dt.datetime.combine(Final_date, Final_time)
+    interval_max = dt.timedelta(days=10)
+    time_difference = date_end - date_begin
+
+    #create boolean check for time interval chosen
+    if time_difference > interval_max:
+        st.write('Its not possible to select more than 11 days.')
+
+
+    
     i = 0
     check = []
     for i in df:
