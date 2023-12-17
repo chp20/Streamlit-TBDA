73b3e853346f44316c573764bea003c30d894027
chp20 <96595434+chp20@users.noreply.github.com>
2023-12-17 21:40:28 +0100

    Update streamlit_plotly_attempt.py

--- streamlit_plotly_attempt.py
+++ streamlit_plotly_attempt.py
@@ -10,7 +10,6 @@ import numpy as np
 ##create switch between initial testing and final dates
 boolean_decision = st.checkbox('Allow me to enter final dates')
 
-
 ##first the final dates part
 if boolean_decision:
     # If checkbox is checked, show the final date inputs
@@ -30,7 +29,6 @@ if boolean_decision:
     final_date_begin = dt.datetime.combine(final_begin_date, final_begin_time)
     final_date_end = dt.datetime.combine(final_end_date, final_end_time)
 
-
     ##connect to mariadb --> phpmyadmin
     connection = mysql.connector.connect(
         host="apiivm78.etsii.upm.es",
@@ -45,12 +43,11 @@ if boolean_decision:
     mycursor.execute(qry)
     rows = mycursor.fetchall()
 
-    
     ##collect all data within timeframe given by user
     for x in rows:
         if final_date_begin <= x[1] and final_date_end >= x[2]:
             checkdata.append(x)
-        
+
     mycursor.close()
     connection.close()
 
@@ -66,112 +63,28 @@ if boolean_decision:
 
     if carrierdf.empty:
         st.warning("No data available for the selected time range.")
-    else: 
+    else:
         fig2 = px.timeline(carrierdf, x_start="Start", x_end="Finish", y="Final_Value", color="Final_Value",
                            color_continuous_scale=[(0, "red"), (1, "green")])
         fig2.update_layout(title_text='Gantt Chart with Final Values')
-    
-        ##plot it
+
+        # plot the Gantt chart
         st.plotly_chart(fig2)
 
+        # Iterate through each data point and create a subplot for each
+        for index, row in carrierdf.iterrows():
+            with st.expander(f'Data Point {index+1}'):
+                # Filter data within the time window of the current data point
+                sub_data = checkdata[(checkdata[:, 1] >= row['Start']) & (checkdata[:, 2] <= row['Finish'])]
 
+                # Plot subplots
+                for sub_index, sub_row in enumerate(sub_data):
+                    plt.figure(figsize=(8, 4))
+                    plt.plot(sub_row[4], sub_row[5], marker='o', linestyle='-', color='b', label='Data Points')
+                    plt.xlabel('X-axis Label')
+                    plt.ylabel('Y-axis Label')
+                    plt.title(f'Subplot for Data Point {index+1} - Subplot {sub_index+1}')
+                    plt.legend()
 
-    ######################################################################################## Temporary workstation
-    # Sample data
-    x_values = np.array([1, 2, 3, 4, 5])
-    y_values = x_values ** 2  # Example: y = x^2
-    
-    # Plotting the line graph
-    fig, ax = plt.subplots()
-    ax.plot(x_values, y_values, marker='o', linestyle='-', color='b', label='Data Points')
-    
-    # Adding labels and title
-    ax.set_xlabel('X-axis Label')
-    ax.set_ylabel('Y-axis Label')
-    ax.set_title('Simple Line Graph')
-    
-    # Adding legend
-    ax.legend()
-    
-    # Display the plot using Streamlit
-    st.pyplot(fig)
-    ###################################################################################### 
-
-
-###########
-###########    initial preliminary plotting
-###########
-else:
-    # If checkbox is not checked, create an empty placeholder
-    placeholder = st.empty()
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        with st.expander('Begin date'):
-            begin_date = st.date_input('Give your begin date:')
-            begin_time = st.time_input('Begin time:')
-
-    with col2:
-        with st.expander('Final date'):
-            final_date = st.date_input('Give your final date:')
-            final_time = st.time_input('Final time:')
-
-    # Create boxes to let the viewer choose what values he wants to see
-    with col3:
-        with st.expander('Values to be plotted'):
-            S_bool = st.checkbox('S_Values')
-            a_bool = st.checkbox('a_values')
-            g_bool = st.checkbox('g_values')
-            m_bool = st.checkbox('m_values')
-
-    # Create time intervals
-    date_begin = dt.datetime.combine(begin_date, begin_time)
-    date_end = dt.datetime.combine(final_date, final_time)
-    interval_max = dt.timedelta(days=10)
-    time_difference = date_end - date_begin
-
-    # Create boolean check for the time interval chosen
-    # if time_difference > interval_max:
-    # st.write('It\'s not possible to select more than 11 days.')
-
-    connection = mysql.connector.connect(
-        host="apiivm78.etsii.upm.es",
-        user="TBDA",
-        password="UPM#2324",
-        database="sclerosisTBDA"
-    )
-
-    mycursor = connection.cursor()
-    qry = "select * FROM `actividad-G02`"
-    mycursor.execute(qry)
-    rows = mycursor.fetchall()
-
-    data = []
-
-    for x in rows:
-        if date_begin <= x[4] <= date_end:
-            # Append a new dictionary to the list
-            data.append({'datetime': x[4], 'time': x[5]})
-
-    # Create a DataFrame from the list
-    df = pd.DataFrame(data)
-
-    # Check if the DataFrame is empty
-    if df.empty:
-        st.warning("No data available for the selected time range.")
-    else:
-        # Create a unitless y-axis column
-        df['unitless_y'] = range(1, len(df) + 1)
-
-        connection.commit()
-        mycursor.close()
-        connection.close()
-
-        # Plotting with Plotly Express
-        fig = px.scatter(df, x='datetime', y='unitless_y')
-
-        # Remove y-axis label (optional)
-        fig.update_yaxes(title_text='')
-
-        # Display the Plotly Express chart
-        st.plotly_chart(fig)
+                    # Display the subplot using Streamlit
+                    st.pyplot(plt.gcf())
