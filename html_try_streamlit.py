c7fd8c80487265f7d8b2d1c76f0c3cf2d025e16f
chp20 <96595434+chp20@users.noreply.github.com>
2023-12-17 18:22:34 +0100

    Update and rename graphana_links.txt to html_try_streamlit.py

---
+++ html_try_streamlit.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# Your Streamlit app code
+
+# Example 1: Using st.markdown
+st.markdown("<h1 style='text-align: center; color: blue;'>Hello Streamlit with HTML</h1>", unsafe_allow_html=True)
