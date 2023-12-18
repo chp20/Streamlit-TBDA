c0c7a96a73f52e6b6c31cd2b2d396c6766162f53
chp20 <96595434+chp20@users.noreply.github.com>
2023-12-18 11:49:40 +0100

    Update streamlit_plotly_attempt.py

--- streamlit_plotly_attempt.py
+++ streamlit_plotly_attempt.py
@@ -1,19 +1,21 @@
-##Import modules
 import streamlit as st
-import pandas as pd
 import datetime as dt
 import mysql.connector
 import plotly.express as px
-import matplotlib.pyplot as plt
-import numpy as np
+import pandas as pd
 
-##create switch between initial testing and final dates
+points = [
+    (dt.datetime(2023, 1, 25, 10, 18, 20), 500, 550, 570, 1000, 1200, 1100, 100, 90, 60, 0.6, 0.65, 0.77),
+    (dt.datetime(2023, 1, 25, 10, 19, 40), 640, 720, 430, 970, 1040, 890, 30, 56, 77, 0.36, 0.78, 0.76),
+    (dt.datetime(2023, 1, 25, 10, 20, 40), 600, 780, 400, 990, 1050, 850, 50, 66, 67, 0.66, 0.68, 0.73)
+]
+
+# Streamlit UI
+st.title('Interactive Plots with Streamlit')
+
 boolean_decision = st.checkbox('Allow me to enter final dates')
 
-
-##first the final dates part
 if boolean_decision:
-    # If checkbox is checked, show the final date inputs
     final_col1, final_col2 = st.columns(2)
 
     with final_col1:
@@ -26,12 +28,9 @@ if boolean_decision:
             final_end_date = st.date_input('Give your final date:', key="final_end_date")
             final_end_time = st.time_input('Final time:', key="final_end_time")
 
-    ##final datetime format chosen dates
     final_date_begin = dt.datetime.combine(final_begin_date, final_begin_time)
     final_date_end = dt.datetime.combine(final_end_date, final_end_time)
 
-
-    ##connect to mariadb --> phpmyadmin
     connection = mysql.connector.connect(
         host="apiivm78.etsii.upm.es",
         user="TBDA",
@@ -45,83 +44,54 @@ if boolean_decision:
     mycursor.execute(qry)
     rows = mycursor.fetchall()
 
-    
-    ##collect all data within timeframe given by user
     for x in rows:
         if final_date_begin <= x[1] and final_date_end >= x[2]:
             checkdata.append(x)
-    st.write(checkdata)
+
     mycursor.close()
     connection.close()
 
-    ##append relevant part of data to new list
     ticker = 0
     checkdata_carrier = []
 
-    ##Set up the plot and add data to right format
     while ticker < len(checkdata):
-        checkdata_carrier.append({"Start": checkdata[ticker][1], "Finish": checkdata[ticker][2], "Final_Value": checkdata[ticker][3]})
+        checkdata_carrier.append({"Start": checkdata[ticker][1], "Finish": checkdata[ticker][2],
+                                  "Final_Value": checkdata[ticker][3]})
         ticker += 1
+
     carrierdf = pd.DataFrame(checkdata_carrier)
 
     if carrierdf.empty:
         st.warning("No data available for the selected time range.")
-    else: 
+    else:
         fig2 = px.timeline(carrierdf, x_start="Start", x_end="Finish", y="Final_Value", color="Final_Value",
                            color_continuous_scale=[(0, "red"), (1, "green")])
         fig2.update_layout(title_text='Gantt Chart with Final Values')
-    
-        ##plot it
         st.plotly_chart(fig2)
 
+    st.subheader("Plots of First Three, Second Three, Third Three, and Fourth Three Sets")
 
-
-    import streamlit as st
-    import matplotlib.pyplot as plt
-    from datetime import datetime
-    
-    # Sample datapoints (replace this with your actual datapoints)
-    points = [
-        (datetime(2023, 1, 25, 10, 18, 20), 500, 550, 570, 1000, 1200, 1100, 100, 90, 60, 0.6, 0.65, 0.77),
-        (datetime(2023, 1, 25, 10, 19, 40), 640, 720, 430, 970, 1040, 890, 30, 56, 77, 0.36, 0.78, 0.76),
-        (datetime(2023, 1, 25, 10, 20, 40), 600, 780, 400, 990, 1050, 850, 50, 66, 67, 0.66, 0.68, 0.73)
-    ]
-    
-    # Function to create plots
-    def create_plots(points):
+    # Function to create Plotly Express plots
+    def create_plotly_express_plots(points):
         num_plots = len(points) // 3  # Adjust this based on your requirement
-    
+
         for i in range(num_plots):
             start_index = i * 3
             end_index = (i + 1) * 3
             subset = points[start_index:end_index]
-    
+
             # Extracting datetime and y values
             datetimes, *y_values = zip(*subset)
-    
+
             # Creating plot
             st.subheader(f'Plot {i + 1}')
-            fig, ax = plt.subplots()
-            for j, y_set in enumerate(y_values):
-                ax.plot(datetimes, y_set, marker='o', label=f'Set {j + 1}')
-    
-            plt.xlabel('Datetime')
-            plt.ylabel('Values')
-            plt.xticks(rotation=45)
-            plt.legend()
-            st.pyplot(fig)
-    
-    # Streamlit UI
-    st.title('Interactive Plots with Streamlit')
-    create_plots(points)
+            df = pd.DataFrame({'Datetime': datetimes, 'Set 1': y_values[0], 'Set 2': y_values[1], 'Set 3': y_values[2]})
+            fig = px.line(df, x='Datetime', y=['Set 1', 'Set 2', 'Set 3'], markers=True)
+            st.plotly_chart(fig)
 
+    create_plotly_express_plots(points)
 
-
-###########
-###########    initial preliminary plotting
-###########
 else:
-    # If checkbox is not checked, create an empty placeholder
     placeholder = st.empty()
     col1, col2, col3 = st.columns(3)
 
@@ -135,7 +105,6 @@ else:
             final_date = st.date_input('Give your final date:')
             final_time = st.time_input('Final time:')
 
-    # Create boxes to let the viewer choose what values he wants to see
     with col3:
         with st.expander('Values to be plotted'):
             S_bool = st.checkbox('S_Values')
@@ -143,15 +112,10 @@ else:
             g_bool = st.checkbox('g_values')
             m_bool = st.checkbox('m_values')
 
-    # Create time intervals
     date_begin = dt.datetime.combine(begin_date, begin_time)
     date_end = dt.datetime.combine(final_date, final_time)
     interval_max = dt.timedelta(days=10)
     time_difference = date_end - date_begin
-
-    # Create boolean check for the time interval chosen
-    # if time_difference > interval_max:
-    # st.write('It\'s not possible to select more than 11 days.')
 
     connection = mysql.connector.connect(
         host="apiivm78.etsii.upm.es",
@@ -169,28 +133,17 @@ else:
 
     for x in rows:
         if date_begin <= x[4] <= date_end:
-            # Append a new dictionary to the list
             data.append({'datetime': x[4], 'time': x[5]})
 
-    # Create a DataFrame from the list
     df = pd.DataFrame(data)
 
-    # Check if the DataFrame is empty
     if df.empty:
         st.warning("No data available for the selected time range.")
     else:
-        # Create a unitless y-axis column
         df['unitless_y'] = range(1, len(df) + 1)
-
         connection.commit()
         mycursor.close()
         connection.close()
-
-        # Plotting with Plotly Express
         fig = px.scatter(df, x='datetime', y='unitless_y')
-
-        # Remove y-axis label (optional)
         fig.update_yaxes(title_text='')
-
-        # Display the Plotly Express chart
         st.plotly_chart(fig)
