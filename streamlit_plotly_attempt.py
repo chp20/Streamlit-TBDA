a85074bfa2bae0aff0037444289edc703179edcc
chp20 <96595434+chp20@users.noreply.github.com>
2023-12-18 11:53:03 +0100

    Update streamlit_plotly_attempt.py

--- streamlit_plotly_attempt.py
+++ streamlit_plotly_attempt.py
@@ -4,11 +4,7 @@ import mysql.connector
 import plotly.express as px
 import pandas as pd
 
-points = [
-    (dt.datetime(2023, 1, 25, 10, 18, 20), 500, 550, 570, 1000, 1200, 1100, 100, 90, 60, 0.6, 0.65, 0.77),
-    (dt.datetime(2023, 1, 25, 10, 19, 40), 640, 720, 430, 970, 1040, 890, 30, 56, 77, 0.36, 0.78, 0.76),
-    (dt.datetime(2023, 1, 25, 10, 20, 40), 600, 780, 400, 990, 1050, 850, 50, 66, 67, 0.66, 0.68, 0.73)
-]
+
 
 # Streamlit UI
 st.title('Interactive Plots with Streamlit')
@@ -71,6 +67,11 @@ if boolean_decision:
 
     st.subheader("Plots of First Three, Second Three, Third Three, and Fourth Three Sets")
 
+    points = [
+    (dt.datetime(2023, 1, 25, 10, 18, 20), 500, 550, 570, 1000, 1200, 1100, 100, 90, 60, 0.6, 0.65, 0.77),
+    (dt.datetime(2023, 1, 25, 10, 19, 40), 640, 720, 430, 970, 1040, 890, 30, 56, 77, 0.36, 0.78, 0.76),
+    (dt.datetime(2023, 1, 25, 10, 20, 40), 600, 780, 400, 990, 1050, 850, 50, 66, 67, 0.66, 0.68, 0.73)
+    ]
     # Function to create Plotly Express plots
     def create_plotly_express_plots(points):
         num_plots = len(points) // 3  # Adjust this based on your requirement
