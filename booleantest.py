ca7399956ed6f0266bb1c2f9a338b5a938cbbdef
chp20 <96595434+chp20@users.noreply.github.com>
2023-12-19 23:06:31 +0100

    Update booleantest.py

--- booleantest.py
+++ booleantest.py
@@ -210,7 +210,8 @@ else:
     dataa = []
 
     for x in data_mdb:
-        if date_begin <= x[4] and x[5] <= date_end:
+        x_datetime = dt.datetime.strptime(x[4], '%Y-%m-%d %H:%M:%S')
+        if date_begin <= x_datetime <= date_end:
             dataa.append({'datetime': x[4], 'time': x[5]})
 
     dff = pd.DataFrame(dataa)
