b70e7f42f051d6da4dcacd727f3e090b113185d3
chp20 <96595434+chp20@users.noreply.github.com>
2023-12-20 14:59:54 +0100

    Update mdbtest.py

--- mdbtest.py
+++ mdbtest.py
@@ -1,1 +1,20 @@
+import 
+connection = mysql.connector.connect(
+        host="apiivm78.etsii.upm.es",
+        user="TBDA",
+        password="UPM#2324",
+        database="sclerosisTBDA"
+    )
 
+    checkdata = []
+    mycursor = connection.cursor()
+    qry = "select * FROM `Data_sample_Christian`"
+    mycursor.execute(qry)
+    rows = mycursor.fetchall()
+
+    for x in rows:
+        if final_date_begin <= x[1] and final_date_end >= x[2]:
+            checkdata.append(x)
+
+    mycursor.close()
+    connection.close()
